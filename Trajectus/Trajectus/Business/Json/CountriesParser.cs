83b8c8fa10efc1bda726e6880c72342e46fb805f
Nicolas Monsegu <Nicolas Monsegu>
2016-07-12 11:23:45 +0200

    added json parser

---
+++ Trajectus/Trajectus/Business/Json/CountriesParser.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Trajectus.Models;
+
+namespace Trajectus.Business.Json
+{
+    public class CountriesParser
+    {
+
+        public string COUNTRIES_PATH = "";
+
+        public CountriesParser()
+        {
+
+        }
+
+        public void LoadData()
+        {
+
+        }
+
+        private string LoadFile(string filename)
+        {
+            string json;
+            using (var sr = new StreamReader(filename))
+            {
+                json = sr.ReadToEnd();
+            }
+            return json;
+        }
+
+        private void LoadWebService()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParseFile(string jsonString)
+        {
+            JArray jsonVal = JArray.Parse(jsonString) as JArray;
+            List<Country> Countries = new List<Country>();
+
+            foreach (dynamic country in jsonVal)
+            {
+                Country newCountry = new Country();
+                newCountry.Name = country.name;
+            }
+        }
+
+        
+
+    }
+}
